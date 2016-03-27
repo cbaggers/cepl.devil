9085653db84a384a3d86ed8bf799f074ea5f0eef
baggers <baggers@cave>
2016-03-27 20:45:32 +0200

    post refactor cleanup

--- devil-helper.lisp
+++ devil-helper.lisp
@@ -7,14 +7,14 @@
       (cl-devil:check-error)
       (let* ((height (cl-devil:image-height))
              (width (cl-devil:image-width))
-             (data-cpy (cffi:foreign-alloc :ubyte-vec4 :count (* height width))))
+             (data-cpy (cffi:foreign-alloc :uint8-vec4 :count (* height width))))
         (cl-devil:copy-pixels 0 0 0 (cl-devil:image-width) (cl-devil:image-height) 1
                               :rgba :unsigned-byte data-cpy)
-        (jungl:make-c-array-from-pointer (list width height)
+        (cepl:make-c-array-from-pointer (list width height)
                                        :ubyte-vec4 data-cpy)))))
 
 (defun load-image-to-texture (filename)
   (let* ((array (load-image-to-c-array filename))
-	 (texture (jungl:make-texture array)))
-    (jungl:free-c-array array)
+	 (texture (cepl:make-texture array)))
+    (cepl:free-c-array array)
     texture))
